541c5aa5a839987a067aec30ee2b28654ad7ef2d
shevtsovak <shevtsovak@yandex.ru>
2021-05-23 17:34:28 +0300

    создан пакет для первой программы

--- sandbox/src/main/java/ru/stqa/pft/sandbox/first_file.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/first_file.java
@@ -1,6 +1,8 @@
-public class hh{   
+package ru.stqa.pft.sandbox;
+
+public class first_file {
 
     public static void main(String[] args) {
     System.out.println("Hello, world!");
     }
-    } 
+    }  
